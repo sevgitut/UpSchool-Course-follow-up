0241ecc1b30bbfcef41bcd9f7916e809359ec162
Sevgi Tut <sevgitut.07@gmail.com>
2023-04-05 01:33:42 +0300

    DbContextOptions and the extension method

--- UpStorage/src/Application/Features/Cities/Commands/Add/CityAddCommandHandler.cs
+++ UpStorage/src/Application/Features/Cities/Commands/Add/CityAddCommandHandler.cs
@@ -6,11 +6,12 @@ using System.Threading.Tasks;
 using Application.Common.Interfaces;
 using Domain.Common;
 using Domain.Entities;
+using Domain.Extensions;
 using MediatR;
 
 namespace Application.Features.Cities.Commands.Add
 {
-    public class CityAddCommandHandler:IRequestHandler<CityAddCommand,Response<int>>
+    public class CityAddCommandHandler : IRequestHandler<CityAddCommand, Response<int>>
     {
         private readonly IApplicationDbContext _applicationDbContext;
 
@@ -21,6 +22,11 @@ namespace Application.Features.Cities.Commands.Add
 
         public async Task<Response<int>> Handle(CityAddCommand request, CancellationToken cancellationToken)
         {
+            if (!request.Name.IsContainsChar(3))
+            {
+                throw new Exception();
+            }
+
             var city = new City()
             {
                 Name = request.Name,
@@ -36,7 +42,7 @@ namespace Application.Features.Cities.Commands.Add
 
             await _applicationDbContext.SaveChangesAsync(cancellationToken);
 
-            return new Response<int>($"The new city named \"{city.Name}\" was successfully added.",city.Id);
+            return new Response<int>($"The new city named \"{city.Name}\" was successfully added.", city.Id);
         }
     }
 }
---
+++ UpStorage/src/Domain/Extensions/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsContainsChar(this string text)
+        {
+            var results = text.Select(x => char.IsLetter(x));
+
+            if (results.Count(x=>x==true))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
