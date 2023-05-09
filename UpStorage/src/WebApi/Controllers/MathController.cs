6f2e2cadd800b7d2565a8249ebd7c3169e2f462c
Sevgi Tut <sevgitut.07@gmail.com>
2023-05-09 22:30:10 +0300

    Unit Testing

---
+++ UpStorage/src/WebApi/Controllers/MathController.cs
@@ -0,0 +1,37 @@
+﻿using Application.Common.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace WebApi.Controllers
+{
+    public class MathController : ApiControllerBase
+    {
+        private readonly MathHelper _mathHelper;
+
+        public MathController(MathHelper mathHelper)
+        {
+            _mathHelper = new MathHelper();
+        }
+
+        [HttpGet]
+        public IActionResult IsEven(int number)
+        {
+            var result= _mathHelper.IsEven(number);
+
+            if (result)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost]
+        public IActionResult Sum(params int[] numbers)
+        {
+            var result = _mathHelper.Sum(numbers);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ UpStorage/tests/Application.UnitTests/Common/Helpers/MathHelperTests.cs
@@ -0,0 +1,71 @@
+﻿using Application.Common.Helpers;
+
+namespace Application.UnitTests.Common.Helpers
+{
+    public class MathHelperTests
+    {
+        [Fact]
+
+        public void IsEven_Returns_True()
+        {
+            //Arrange
+            var mathHelper = new MathHelper();
+
+            int evenNumber = 6;
+            int secondEvenNumber = 82;
+
+            //Act
+            var result = mathHelper.IsEven(evenNumber);
+
+            var secondResult = mathHelper.IsEven(secondEvenNumber);
+
+            //Assert
+            Assert.True(result);
+            Assert.True(secondResult);
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+
+        public void IsEven_Returns_False()
+        {
+            //Arrange
+            var mathHelper = new MathHelper();
+
+            int oddNumber = 1;
+            int secondOddNumber = 11;
+
+            //Act
+            var result = mathHelper.IsEven(oddNumber);
+
+            var secondResult = mathHelper.IsEven(secondOddNumber);
+
+            //Assert
+            Assert.False(result);
+            Assert.False(secondResult);
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+
+        public void Add_Returns_True()
+        {
+            //Arrange
+            var mathHelper = new MathHelper();
+
+            int firstNumber = 13;
+            int secondOddNumber = 18;
+
+            //Act
+            var result = mathHelper.Add(firstNumber,secondOddNumber);
+
+
+            //Assert
+            Assert.Equal(31,result);
+
+            Assert.NotEqual(5,result);
+
+            
+        }
+    }
+}
